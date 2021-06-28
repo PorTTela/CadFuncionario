261feb3bab564fbbb8cf66500a0eaa00a2c5b134
PorTTela <thiago.portela@ymail.com>
2021-06-28 12:57:32 -0300

    Adicionar arquivos de projeto.

---
+++ CadFuncionario/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+
+namespace CadFuncionario
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Funcionario(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void AumentarSalario (double percentual)
+        {
+            Salary += Salary / 100 * percentual;
+        }
+
+        public override string ToString()
+        {
+            return "ID: " + Id + ", Nome: " + Name + ", Salário: " + Salary.ToString("F2", CultureInfo.InvariantCulture) + "R$" ;
+        }
+
+    }
+}
---
+++ CadFuncionario/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace CadFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Quantos funcionários você deseja registrar? ");
+            int numfuncionario = int.Parse(Console.ReadLine());
+
+            List<Funcionario> listFunc = new List<Funcionario>();
+
+            for (int i = 0; i < numfuncionario; i++)
+            {
+                Console.WriteLine("Funcionário " + i + ": ");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salário: ");
+                double salario = double.Parse(Console.ReadLine());
+                listFunc.Add(new Funcionario(id, name, salario));
+                Console.WriteLine();
+            }
+
+            Console.Write("Digite o código do funcionário que deseja aumentar o salário: ");
+            int codigoAumento = int.Parse(Console.ReadLine());
+
+            Funcionario funcId = listFunc.Find(x => x.Id == codigoAumento);
+            if (funcId!= null)
+            {
+                Console.Write("Deseja aumentar o salário em quantos porcentos? ");
+                double porcAumento = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                funcId.AumentarSalario(porcAumento);
+            }
+            else
+            {
+                Console.WriteLine("O Código não existe!");
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("Os funcionários cadastrados são:");
+            foreach (Funcionario obj in listFunc)
+            {
+                Console.WriteLine(obj);
+
+
+            }
+        }
+    }
+}
